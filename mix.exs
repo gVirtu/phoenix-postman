605d1d01572e8698ed3580cf7f51dd477586c052
Giancarlo <giandroberto@yahoo.com.br>
2018-05-19 19:03:17 -0300

    Add ExCoveralls

--- mix.exs
+++ mix.exs
@@ -1,6 +1,8 @@
 defmodule PhoenixPostman.Mixfile do
   use Mix.Project
 
+  @test_envs [:dev, :test]
+
   def project do
     [
       app: :phoenix_postman,
@@ -8,6 +10,7 @@ defmodule PhoenixPostman.Mixfile do
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
+      test_coverage: [tool: ExCoveralls],
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -24,7 +27,7 @@ defmodule PhoenixPostman.Mixfile do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(env) when env in @test_envs, do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
@@ -32,6 +35,7 @@ defmodule PhoenixPostman.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:excoveralls, "~> 0.8.2", only: @test_envs},
       {:phoenix, "~> 1.3.2"},
       {:postgrex, ">= 0.0.0"},
       {:cowboy, "~> 1.0"}
